bd4e83ddd504ae05f4c20e3a72ead28a7149589c
Emre Samurlu <emrsmrl@gmail.com>
2021-12-02 03:14:11 +0300

    C# Console Programlama Dersi

--- console-programming/Program.cs
+++ console-programming/Program.cs
@@ -2,11 +2,17 @@
 
 namespace console_programming
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Console.WriteLine("Hello World!");
+      Console.WriteLine("İsminizi girin:");
+      string name = Console.ReadLine();
+      Console.WriteLine("Soyadınızı Girin!");
+      string surname = Console.ReadLine();
+
+      Console.WriteLine("Merhaba " + name + " " + surname);
     }
+  }
 }
